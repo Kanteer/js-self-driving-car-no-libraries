09746e594a5652f286f5094c898157e107ce7ea0
kanteer <59344076+Kanteer@users.noreply.github.com>
2022-09-15 10:08:57 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
+const canvas=document.getElementById("myCanvas");
+canvas.height=window.innerHeight;
+canvas.width=200;
 
+const ctx = canvas.getContext("2d");
+const car=new Car(100,10030,50)
+car.draw(ctx);
