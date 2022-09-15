88ff874f84012ac372554c6fa769cbbf59ffd395
kanteer <59344076+Kanteer@users.noreply.github.com>
2022-09-15 10:30:53 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,4 +4,11 @@ canvas.width=200;
 
 const ctx = canvas.getContext("2d");
 const car=new Car(100,10030,50)
-car.draw(ctx);
+
+animate();
+
+function animate(){
+  car.update();
+  car.draw(ctx);
+  requestAnimationFrame(animate);
+}
